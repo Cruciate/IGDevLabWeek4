f0e58e44a46ca8de3f5ecda89c3d1fd456ed8acf
Matt <matt206690@gmail.com>
2022-08-29 03:43:54 +1000

    100% done :)

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -8,16 +8,33 @@ public class PrintAndHide : MonoBehaviour
     //public GameObject gameObject;
     int i = 3;
     public Renderer rend;
+    public int randomint;
+    
 
     void Start()
     {
-        
+        randomint = Random.Range(200, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        
+            
+        
         i++;
         Debug.Log(gameObject.name + ":" + i.ToString());
+
+        if((gameObject.tag == "Red") && (i == 100)){
+            gameObject.SetActive(false);
+        }
+
+
+        
+        if((gameObject.tag == "Blue") && (i == randomint)){
+            rend.enabled = false;
+            
+        }
     }
 }
